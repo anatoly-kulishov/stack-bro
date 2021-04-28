90eb4552585cc4b60ac0aacf2ad5ec7c29083b9b
RewQ <rewq_gol_47@mail.ru>
2021-04-28 18:54:51 +0300

    1.0.0

--- react-kabzda-1/src/index.js
+++ react-kabzda-1/src/index.js
@@ -4,14 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const app = (
     <StrictMode>
         <App/>
-    </StrictMode>,
-    document.getElementById('root')
-);
+    </StrictMode>
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+ReactDOM.render(app, document.getElementById('root'));
+
 reportWebVitals();
