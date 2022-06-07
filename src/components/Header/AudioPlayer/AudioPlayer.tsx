ad9b08ca46ec23eca94132e5aaea5747e194c2f4
Anatoly.Kulishov <Anatoly.Kulishov@waveaccess.ru>
2022-06-07 15:50:35 +0300

    3.3.1

--- src/components/Header/AudioPlayer/AudioPlayer.tsx
+++ src/components/Header/AudioPlayer/AudioPlayer.tsx
@@ -6,7 +6,13 @@ import song from './my_song.mp3';
 export const AudioPlayer: FC = () => {
   return (
     <div className="d-flex align-items-center">
-      <ReactAudioPlayer src={song} style={{ height: '30px', marginLeft: '20px' }} volume={1.0} controls />
+      <ReactAudioPlayer
+        src={song}
+        style={{ height: '30px', marginLeft: '20px' }}
+        volume={1.0}
+        controls
+        autoPlay={true}
+      />
     </div>
   );
 };
